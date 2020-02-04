0df0b7d087bf4b5489c981b532ff36bddd53cae7
Ajitesh Abhishek <ajiteshleo@gmail.com>
2020-02-04 14:21:09 -0600

    first commit

---
+++ backend/src/api.py
@@ -0,0 +1,152 @@
+import os
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+from flask_cors import CORS
+
+from .database.models import db_drop_and_create_all, setup_db, Drink, db
+from .auth.auth import AuthError, requires_auth
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+'''
+@TODO uncomment the following line to initialize the datbase
+!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
+!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
+'''
+#db_drop_and_create_all()
+
+
+#------------------------------------------------------------
+#  Controllers
+#-------------------------------------------------------------
+
+@app.route('/drinks')
+def get_drinks():
+    '''
+    Get drinks in the drink.short() data representation
+    '''
+    drinks = Drink.query.all()
+
+    if len(drinks) == 0:
+        abort(404)
+
+    formatted_drinks = [drink.short() for drink in drinks]
+
+    return jsonify({
+        'success': True,
+        'drinks': formatted_drinks
+        })
+
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drink_detail(jwt):
+    '''
+    Fetch the long form representation of the drinks
+    '''
+    drinks = Drink.query.all()
+
+    if len(drinks) == 0:
+        abort(404)
+
+    formatted_drinks = [drink.long() for drink in drinks]
+
+    return jsonify({
+        'success': True,
+        'drinks': formatted_drinks
+        })
+
+
+'''
+@TODO implement endpoint
+    POST /drinks
+        it should create a new row in the drinks table
+        it should require the 'post:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+        or appropriate status code indicating reason for failure
+'''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_detail(jwt):
+    '''
+    Add a new drink in the drinks table
+    '''
+    body = request.get_json()
+    new_title = body.get('title', None)
+    recipe_json = body.get('recipe', None)
+
+    new_drink = Drink(
+                    title = new_title,
+                    recipe = json.dumps(recipe_json)
+                )
+
+    db.session.add(new_drink)
+    db.session.commit()
+
+    return jsonify({
+        'success': True,
+        'drinks': new_drink.long()
+        })
+
+
+'''
+@TODO implement endpoint
+    PATCH /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should update the corresponding row for <id>
+        it should require the 'patch:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+        or appropriate status code indicating reason for failure
+'''
+
+
+'''
+@TODO implement endpoint
+    DELETE /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should delete the corresponding row for <id>
+        it should require the 'delete:drinks' permission
+    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+        or appropriate status code indicating reason for failure
+'''
+
+
+## Error Handling
+'''
+Example error handling for unprocessable entity
+'''
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+                    "success": False,
+                    "error": 422,
+                    "message": "unprocessable"
+                    }), 422
+
+'''
+@TODO implement error handlers using the @app.errorhandler(error) decorator
+    each error handler should return (with approprate messages):
+             jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+
+'''
+
+'''
+@TODO implement error handler for 404
+    error handler should conform to general task above
+'''
+
+
+'''
+@TODO implement error handler for AuthError
+    error handler should conform to general task above
+'''
